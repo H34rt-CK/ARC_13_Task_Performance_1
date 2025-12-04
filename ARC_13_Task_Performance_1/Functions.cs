23dfeaae67808d9b334406b4791be2ba96260bda
Aaron Salangsang <salangsangaaron905@gmail.com>
2025-12-04 16:13:50 +0800

    Test code omsim

--- ARC_13_Task_Performance_1/Functions.cs
+++ ARC_13_Task_Performance_1/Functions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,35 +10,33 @@ namespace ARC_13_Task_Performance_1
 {
     internal class Functions
     {
-        public static string? Create(string txtCreate)
+        
+        string conn = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        void Admin()
         {
-            try
+            bool isAdmin = false;
+            if (isAdmin == true)
             {
-                using var saveFileDialog = new System.Windows.Forms.SaveFileDialog
-                {
-                    Title = "Create Attendance File",
-                    //Filter = "Text Files (*.txt)|*.txt|CSV Files (*.csv)|*.csv|All Files (*.*)|*.*",
-                    Filter = "INI Files (*.ini)|*.ini|All Files (*.*)|*.*",
-                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                    FileName = "attendance.txt",
-                    DefaultExt = "txt"
-                };
+                Create();
+                Read();
+                Update();
+                Delete();
+            }
+        }
 
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog.FileName))
-                    {
-                        // Write some initial content to the file
-                        file.WriteLine("Test File");
-                        //txtCreate.Text = saveFileDialog.FileName; no textbox yet
-                    }
-                }
-            }
-            catch (Exception ex)
+        void Create()
+        {
+        using (SqlConnection con = new SqlConnection(conn))
             {
-                Console.WriteLine("An error occurred while creating the file: " + ex.Message);
+                string query = "INSERT INTO Recipes (Title, Ingredients, Instructions) VALUES (@t, @i, @s)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@t", "Sample Recipe");
+                cmd.Parameters.AddWithValue("@i", "Sample Ingredients");
+                cmd.Parameters.AddWithValue("@s", "Sample Instructions");
+                con.Open();
+                cmd.ExecuteNonQuery();
             }
-            return null;
+            Console.WriteLine("Recipe Added");
         }
         void Read()
         {

e649daf1a170ae07950eede45d6f908b7d9d5fbe
Aaron Salangsang <salangsangaaron905@gmail.com>
2025-12-05 21:12:22 +0800

    Added Create function

--- ARC_13_Task_Performance_1/Functions.cs
+++ ARC_13_Task_Performance_1/Functions.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
-using Microsoft.Data.SqlClient;
+using System.Diagnostics.Metrics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ARC_13_Task_Performance_1
 {
     internal class Functions
     {
         
-        string conn = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        string conn = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = RecipeDB; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False";
         void Admin()
         {
             bool isAdmin = false;
@@ -24,16 +27,24 @@ namespace ARC_13_Task_Performance_1
             }
         }
 
-        void Create()
+        public void Create()
         {
-        using (SqlConnection con = new SqlConnection(conn))
+            string name, ingredients, instructions;
+            Console.Write("Enter Recipe Name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter Ingredients: ");
+            ingredients = Console.ReadLine();
+            Console.Write("Enter Instructions: ");
+            instructions = Console.ReadLine();
+
+            using (SqlConnection con = new SqlConnection(conn))
             {
-                string query = "INSERT INTO Recipes (Title, Ingredients, Instructions) VALUES (@t, @i, @s)";
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@t", "Sample Recipe");
-                cmd.Parameters.AddWithValue("@i", "Sample Ingredients");
-                cmd.Parameters.AddWithValue("@s", "Sample Instructions");
                 con.Open();
+                var cmd = new SqlCommand(
+                    "INSERT INTO Recipes (Title, Ingredients, Instructions) VALUES (@ttl, @ing, @ins)", con);
+                cmd.Parameters.AddWithValue("@ttl", name);
+                cmd.Parameters.AddWithValue("@ing", ingredients);
+                cmd.Parameters.AddWithValue("@ins", instructions);
                 cmd.ExecuteNonQuery();
             }
             Console.WriteLine("Recipe Added");

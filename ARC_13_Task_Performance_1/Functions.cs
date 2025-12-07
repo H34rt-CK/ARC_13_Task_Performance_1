e5d8cd9625e5f02f080a404ca1d6f260c8681b31
Aaron Salangsang <salangsangaaron905@gmail.com>
2025-12-07 10:06:03 +0800

    added CRUD

--- ARC_13_Task_Performance_1/Functions.cs
+++ ARC_13_Task_Performance_1/Functions.cs
@@ -15,51 +15,125 @@ namespace ARC_13_Task_Performance_1
     {
         
         string conn = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = RecipeDB; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False";
-        void Admin()
+        public void Admin()
         {
             bool isAdmin = false;
-            if (isAdmin == true)
-            {
-                Create();
-                Read();
-                Update();
-                Delete();
-            }
+            //if (isAdmin == true)
+            //{
+                Console.WriteLine("\nADMIN MENU");
+                Console.WriteLine("1. Create");
+                Console.WriteLine("2. Read");
+                Console.WriteLine("3. Update");
+                Console.WriteLine("4. Delete");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1": Create(); break;
+                    case "2": Read(); break;
+                    case "3": Update(); break;
+                    case "4": Delete(); break;
+                    case "5": return;
+                    default: Console.WriteLine("Invalid."); break;
+                }
+            //}
         }
 
         public void Create()
         {
-            string name, ingredients, instructions;
+            string name, ingredients, instructions, recipeText;
             Console.Write("Enter Recipe Name: ");
             name = Console.ReadLine();
             Console.Write("Enter Ingredients: ");
             ingredients = Console.ReadLine();
             Console.Write("Enter Instructions: ");
             instructions = Console.ReadLine();
-
+            recipeText = $"Recipe: {name}\nIngredients: {ingredients}\nInstructions: {instructions}";
             using (SqlConnection con = new SqlConnection(conn))
             {
                 con.Open();
                 var cmd = new SqlCommand(
-                    "INSERT INTO Recipes (Title, Ingredients, Instructions) VALUES (@ttl, @ing, @ins)", con);
+                    "INSERT INTO Recipes (Title, RecipeText) VALUES (@ttl, @txt)", con);
                 cmd.Parameters.AddWithValue("@ttl", name);
-                cmd.Parameters.AddWithValue("@ing", ingredients);
-                cmd.Parameters.AddWithValue("@ins", instructions);
+                cmd.Parameters.AddWithValue("@txt", recipeText);
                 cmd.ExecuteNonQuery();
             }
             Console.WriteLine("Recipe Added");
         }
-        void Read()
+        public List<Recipe> Read()
         {
-
+            List<Recipe> recipes = new List<Recipe>();
+            using (SqlConnection con = new SqlConnection(conn))
+            {
+                con.Open();
+                var cmd = new SqlCommand("SELECT Id, Title, RecipeText FROM Recipes", con);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    recipes.Add(new Recipe
+                    {
+                        Id = reader.GetInt32(0),
+                        Title = reader.GetString(1),
+                        RecipeText = reader.GetString(2)
+                    });
+                }
+            }
+            return recipes;
         }
-        void Update()
+        public void Update()
         {
-
+            int id;
+            string name, ingredients, instructions, recipeText;
+            Console.Write("Enter Recipe ID to Update: ");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter New Recipe Name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter New Ingredients: ");
+            ingredients = Console.ReadLine();
+            Console.Write("Enter New Instructions: ");
+            instructions = Console.ReadLine();
+            recipeText = $"Recipe: {name}\nIngredients: {ingredients}\nInstructions: {instructions}";
+            using (SqlConnection con = new SqlConnection(conn))
+            {
+                con.Open();
+                var cmd = new SqlCommand(
+                    "UPDATE Recipes SET Title = @ttl, RecipeText = @txt WHERE Id = @id", con);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@ttl", name);
+                cmd.Parameters.AddWithValue("@txt", recipeText);
+                cmd.ExecuteNonQuery();
+            }
         }
-        void Delete()
+        public void Delete()
         {
-
+            Console.Write("Enter Recipe ID to delete: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            using (SqlConnection con = new SqlConnection(conn))
+            {
+                con.Open();
+                var cmd = new SqlCommand(
+                    "DELETE FROM Recipes WHERE Id = @id", con);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    Console.WriteLine("Recipe Deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Recipe Not Found");
+                }
+            }
         }
+    }
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string RecipeText { get; set; }
     }
 }
